57d4ba8c96eaec55034f06d379a095d25c169f14
Gabe Levine <gabe@g4briel.com>
2017-10-04 17:34:00 -0400

    adding keys to parsers

--- parsers/midi_to_hex_dir.py
+++ parsers/midi_to_hex_dir.py
@@ -5,7 +5,6 @@ from mido import MidiFile
 
 def parse_single_midi_file_with_time(infile, outfile):
     try:
-        print("here")
         for msg in MidiFile(infile):
             if msg.time != 0:
                 outfile.write(str(msg.hex()) + "," + str(msg.time) + '\n')
@@ -19,11 +18,14 @@ def parse_single_midi_file_with_time(infile, outfile):
 
 
 if __name__ == "__main__":
+    num_files = 1
     with open(sys.argv[2], 'a+') as outfile:
         directory = sys.argv[1]
         for filename in os.listdir(directory):
             try:
                 parse_single_midi_file_with_time(directory + "/" + filename, outfile)
+                print(num_files)
+                num_files += 1
             except Exception as e:
                 print e.message
                 continue
